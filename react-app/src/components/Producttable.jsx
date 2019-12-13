bf4c6ed6a5ef3fbfabe0686f3c2eec9f407d61c6
Kealan Crilly <D00224427@student.dkit.ie>
2019-12-13 15:01:17 +0000

    ytea

--- react-app/src/components/Producttable.jsx
+++ react-app/src/components/Producttable.jsx
@@ -1,16 +1,43 @@
 import React    from 'react';
+import Table from 'react-bootstrap/table'; 
 
 class Producttable extends React.Component {
 
   render() {
     return (
-      <div className='jumbotron'>
-        <h1 className='display-4'>Drinks Shop</h1>
-        <p>This is where a table is</p>
-      </div> 
+      <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
     );
   }
 
 }
+
 
 export default Producttable;
