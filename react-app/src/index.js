2a771014ad428d58410fda5a18e49969a8d45bcd
Kealan Crilly <D00224427@student.dkit.ie>
2019-12-14 12:46:22 +0000

    d

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 import App      from './components/App';
-import './lib/styles/main.scss';
+import './lib./sass/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
