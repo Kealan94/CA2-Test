7075bc9450a71171a37bbf4131b89ec15ba4c01f
Kealan Crilly <crilly2k12@hotmail.co.uk>
2019-12-14 18:33:26 +0000

    fff

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,27 +2,5 @@ import React    from 'react';
 import ReactDOM from 'react-dom';
 import App      from './components/App';
 import './components/styles/main.css';
-import StripeCheckout from 'react-stripe-checkout';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-function APP() {
-    const {product} = React.useState({
-        name: "Magners",
-        price: 12.00,
-    });
-}
-function handleToken(token, addresses){
-console.log ({token, addresses})
-return (
-    <div className = "container">
-    <div className = "product">
-    <h1>(product.name}</h1>
-    <h3>On Sale ${product.price}</h3>
-    </div>
-    <StripeCheckout
-    stripeKey="pk_test_ZlLTPrZQWlcIwfmwkLLspBBr007URNccK3"
-    token={handleToken}
-    </div>
-)
