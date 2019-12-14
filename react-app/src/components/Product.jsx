6704d41503af5bad8925c7e7a34738565d19372f
Kealan Crilly <D00224427@student.dkit.ie>
2019-12-14 14:03:04 +0000

    ggg

--- react-app/src/components/Product.jsx
+++ react-app/src/components/Product.jsx
@@ -25,6 +25,9 @@ class Product extends React.Component {
           <div className='col-12 col-md-9'>
             <div className='card-body'>
               <h3 className='card-sub-title'>&euro;{(parseFloat(this.props.product.price)).toFixed(2)}</h3>
+              <ul>{this.props.product.description.split("\n").map(each => (
+                <li>{each}</li>
+              ))}
               <p className='card-text'>{this.props.product.description}</p>
               <AddToCartButton product={this.props.product} cart={this.props.cart} />
             </div>
