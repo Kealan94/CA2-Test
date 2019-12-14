e8b123cad519627b3736b910835cf381b1f16d3f
Kealan Crilly <D00224427@student.dkit.ie>
2019-12-14 10:49:37 +0000

    y

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,5 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 import App      from './components/App';
-import "./styles/partials/_variables.scss";
 
 ReactDOM.render(<App />, document.getElementById('root'));
