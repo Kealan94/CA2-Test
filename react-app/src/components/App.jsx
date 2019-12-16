b0579bb45427a35c5d2c93eda8dfaa877a0d4371
Kealan Crilly <D00224427@student.dkit.ie>
2019-12-16 15:17:04 +0000

    gg

--- react-app/src/components/App.jsx
+++ react-app/src/components/App.jsx
@@ -6,7 +6,8 @@ import Cart         from '../lib/Cart';
 import Stripe       from './Stripe';
 import Form         from './Form';
 import Dropdown     from './Dropdown';
-import Tables        from './Tables';
+import InputGroup   from './InputGroup';
+
  
  
  
@@ -29,7 +30,7 @@ class App extends React.Component {
         <Stripe />   
         <Footer />
        <Dropdown />
-       <Tables />
+       <InputGroup />
 
        
       </div>
