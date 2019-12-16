5248b070660c04c0e8d0213734835b194f1928ca
Kealan Crilly <D00224427@student.dkit.ie>
2019-12-16 10:24:16 +0000

    dd

--- react-app/src/components/App.jsx
+++ react-app/src/components/App.jsx
@@ -5,7 +5,6 @@ import Footer       from './Footer';
 import Cart         from '../lib/Cart';
 import Stripe       from './Stripe';
 import Form         from './Form';
-import Navbar       from './NavBar';
  
  
  
@@ -28,7 +27,6 @@ class App extends React.Component {
         <Stripe />   
         <Footer />
         <Form   />
-        <Navbar />
        
       </div>
     );
