6ab04beb01ddda558e4b063d4a89493979cfc318
Kealan Crilly <crilly2k12@hotmail.co.uk>
2019-12-16 22:46:52 +0000

    aefa

---
+++ react-app/src/components/App.jsx
@@ -0,0 +1,32 @@
+import React        from 'react';
+import ProductsList from './ProductList';
+import Header       from './Header';
+import Footer       from './Footer';
+import Stripe       from './Stripe';
+import Cart         from '../lib/Cart';
+
+class App extends React.Component {
+
+  // Note the cart object is just a vanilla JS
+  // custom type (see lib/Cart.js)
+  state = {
+    cart : new Cart()
+  }
+
+  // Render
+  render() {
+    return (
+      <div className='App'>
+        <Header cart={this.state.cart} />
+        <ProductsList cart={this.state.cart} />
+        <Stripe />
+        <center><h1>Be safe please don't drink and drive.</h1></center>
+
+        <Footer />
+       
+      </div>
+    );
+  }
+}
+
+export default App;
